5378bc09efe28cbf6d3f7e976ce1fa14082e1b4b
c0a22073a1 <c0a22073a1@edu.teu.ac.jp>
2023-11-28 14:23:38 +0900

    練習2爆弾surfaceに移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,6 +6,13 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1000, 600
 
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -19,6 +26,7 @@ def main():
     bb_rct = bb_img.get_rect()  # 練習2
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -28,6 +36,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
